4550ab6c7d806473cb09923b139e868c469b7ef9
Ma1y0 <matyas.barr@gmail.com>
2023-01-05 10:17:20 +0100

    GitHUb OAuth working

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,36 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github"
+
+import { env } from "../../../env/server.mjs"
+import { prisma } from "../../../server/db/client"
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id
+      }
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    })
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
